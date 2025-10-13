5c623967fc37aaa2aa16b6ccf834199b802836c7
ano201 <ahmu689@gmail.com>
2025-10-14 05:58:19 +0600

    feat: added initial CLI menu system

---
+++ src/PasswordEntryManager.cpp
@@ -0,0 +1,26 @@
+#include "PasswordEntryManager.h"
+
+void PasswordEntryManager::addNewEntry()
+{
+    cout << "You selected: Add New Password Entry.\n";
+}
+
+void PasswordEntryManager::viewAllEntries()
+{
+    cout << "You selected: View All Passwords.\n";
+}
+
+void PasswordEntryManager::searchPassword()
+{
+    cout << "You selected: Search Password By Name/Website.\n";
+}
+
+void PasswordEntryManager::deletePassword()
+{
+    cout << "You selected: Delete A Password Entry.\n";
+}
+
+void PasswordEntryManager::updatePassword()
+{
+    cout << "You selected: Update/Edit Existing Password.\n";
+}
